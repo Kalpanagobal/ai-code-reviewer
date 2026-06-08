98a271ef1ed24ea874e06fa0bee01e5e6a4a69fc
Kalpana Gopal <kalpanagobal2006@gmail.com>
2026-06-08 22:28:31 +0530

    Update review.py

--- review.py
+++ review.py
@@ -1,23 +1,3 @@
-from groq import Groq
 import os
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
-
-response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
-    messages=[
-        {
-            "role": "user",
-            "content": """
-Review this C# code and tell me the issues:
-
-string name = null;
-Console.WriteLine(name.Length);
-"""
-        }
-    ]
-)
-
-print(response.choices[0].message.content)
+print("API KEY:", os.getenv("GROQ_API_KEY"))
