7f06b8495c09842a33acf1cb8a4a2b4c44a60de5
Kalpana Gopal <kalpanagobal2006@gmail.com>
2026-06-13 12:10:01 +0530

    Add bug identification in Program class
    Identified multiple bugs in the Program class, including index out of range, infinite loop, null reference, and overflow.

---
+++ bug.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> fruits = new List<string>();
+
+        fruits.Add("Apple");
+        fruits.Add("Banana");
+        fruits.Add("Mango");
+
+        // Bug 1: Index out of range
+        Console.WriteLine(fruits[3]);
+
+        // Bug 2: Wrong loop condition (infinite loop)
+        int i = 0;
+        while (i < fruits.Count)
+        {
+            Console.WriteLine(fruits[i]);
+        }
+
+        // Bug 3: Null reference
+        string message = null;
+        Console.WriteLine(message.Trim());
+
+        // Bug 4: Overflow possibility
+        byte num = 255;
+        num++;
+
+        Console.WriteLine("Number: " + num);
+    }
+}
